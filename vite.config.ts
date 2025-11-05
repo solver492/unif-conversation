9faaca6580ee7bd8399db94e2d6ebdf2d5ef6970
solver492 <digitalsolverland@gmail.com>
2025-11-05 23:13:20 +0100

    Add allowedHosts for Render domainnow tha work
    Add allowed host for Render domain in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['unif-conversation.onrender.com'] // Ajoute ici le domaine Render
       },
       plugins: [react()],
       define: {
